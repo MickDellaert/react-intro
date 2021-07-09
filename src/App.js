c50ea0bcb543b1603df3d9805a63d39114525966
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-07-09 12:17:15 +0200

    local storage working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 import Header from "./Header";
 import ToDoInputComp from "./ToDoInput/ToDoInputComp";
 import ToDoListComp from "./ToDoList/ToDoListComp";
 
+const LSKEY = 'MyTodoApp'
+
 const App = () => {
 
     const initialTodos = [
-        { id: 1, name: "My first todo", complete: true},
+        {id: 1, name: "My first todo", complete: true},
         {id: 2, name: "My second todo", complete: false},
         {id: 3, name: "My third todo", complete: false}
-
     ];
 
     const [todos, setTodos] = useState(initialTodos);
 
+    useEffect(()=> {
+        const storeditems = JSON.parse(localStorage.getItem(LSKEY + ".todos"))
+        if (storeditems) setTodos(storeditems)
+    }, [])
+
+
+    useEffect(() => {
+        window.localStorage.setItem(LSKEY + ".todos", JSON.stringify(todos));
+    }, [todos]);
+
     return (
+
         <div className="App">
             <Header/>
             <div className="content">
--- src/ToDoInput/ToDoInputComp.js
+++ src/ToDoInput/ToDoInputComp.js
@@ -1,4 +1,5 @@
 import React, {useRef} from 'react';
+import { v4 as uuid4 } from 'uuid';
 
 import ToDoInputButton from "./ToDoInputButton";
 import ToDoInputField from "./ToDoInputField";
@@ -9,20 +10,22 @@ const ToDoInputComp = ({todos, setTodos}) => {
     const addTodo = () => {
 
         // Adding a new to do from the inputfield using the useRef hook
-        let newTodo = inputRef.current.value
+        let newTodo = inputRef.current.value;
+
+        // Clear inputfield
+        if (newTodo === '') return;
+        inputRef.current.value = null;
 
         // First making a clone and adding a new to do to the new state, still need to change the key so it's a unique value TODO
-        let newTodos = [...todos, {id: 4, name: newTodo, complete: false}]
+        setTodos([...todos, {id: uuid4(), name: newTodo, complete: false}])
 
         // Not sure if I am making a clone in a good way here, also not sure about the JSON.parse TODO
         // let parseTodos = JSON.parse(JSON.stringify(newTodos));
         // newTodos.push({id: 4, name: newTodo.value, complete: false})
-
-        setTodos(newTodos)
         }
 
 
-    // Got stuck here for a very long time, just trying to access the addTodo function from the onClick inside the ToDoInputButton child component and displaying a console.log message, turns out it was because of the return in here, the parent, being wrapped inside a form tag. Replacing it by a regular div made it work. Need to look into this TODO
+    // Got stuck here for a very long time, trying to access the addTodo function from the onClick inside the ToDoInputButton child component and displaying a console.log message, turns out it was because of the return in here, the parent, being wrapped inside a form tag. Replacing it by a regular div made it work. Need to look into this TODO
     return (
         <div className="todo-form-container">
         <div>
